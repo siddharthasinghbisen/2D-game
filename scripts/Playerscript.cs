dcc30e326dc18dd2f1d75820b355dde5ece73c57
siddharth <pscrusher101@gmail.com>
2018-04-20 05:45:41 +0200

    Add files via upload

---
+++ scripts/Playerscript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playerscript : MonoBehaviour {
+
+    public float jumpPower = 10.0f;
+    Rigidbody2D myRIgidbody;
+	// Use this for initialization
+	void Start () {
+        myRIgidbody = transform.GetComponent<Rigidbody2D>();
+	}
+	
+
+	void FixedUpdate () {
+		
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRIgidbody.AddForce(Vector3.up * (jumpPower * myRIgidbody.mass * myRIgidbody.gravityScale * 20.0f));
+        }
+	}
+}
