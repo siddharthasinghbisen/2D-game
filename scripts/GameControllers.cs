7a76ca6cb6efcd4007c3c12e216865899c2a2a8d
siddharth <pscrusher101@gmail.com>
2018-04-23 03:19:00 +0200

    Add files via upload

---
+++ scripts/GameControllers.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControllers : MonoBehaviour
+{
+    public GameObject gameOverPanel;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void GameOver()
+    {
+        Invoke("ShowOverPanel", 2.0f);
+    }
+    void ShowOverPane()
+    {
+        gameOverPanel.SetActive(true);
+    }
+}
--- scripts/Playerscript.cs
+++ scripts/Playerscript.cs
@@ -5,18 +5,65 @@ using UnityEngine;
 public class Playerscript : MonoBehaviour {
 
     public float jumpPower = 10.0f;
-    Rigidbody2D myRIgidbody;
+    Rigidbody2D myRigidbody;
+    bool isGrounded = false;
+    float posX = 0.0f;
+    bool isGameOver = false;
+    ChallengeController myChallengeController;
 	// Use this for initialization
 	void Start () {
-        myRIgidbody = transform.GetComponent<Rigidbody2D>();
+        myRigidbody = transform.GetComponent<Rigidbody2D>();
+        posX = transform.position.x;
+        myChallengeController = GameObject.FindObjectOfType<ChallengeController>();
 	}
 	
 
 	void FixedUpdate () {
 		
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && isGrounded && !isGameOver)
         {
-            myRIgidbody.AddForce(Vector3.up * (jumpPower * myRIgidbody.mass * myRIgidbody.gravityScale * 20.0f));
+            myRigidbody.AddForce(Vector3.up * (jumpPower * myRigidbody.mass * myRigidbody.gravityScale * 20.0f));
+      
         }
-	}
+        //collision check
+        if (transform.position.x < posX)    
+        {
+            GameOver(); 
+        }
+    }
+    private void Update()
+    {
+
+    }
+        void GameOver()
+        {
+        isGameOver = true;
+        myChallengeController.GameOver();
+        }
+    
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+    void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.collider.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+        if(other.collider.tag == "Enemy")
+        {
+            GameOver();
+        }
+    }
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.collider.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
 }
